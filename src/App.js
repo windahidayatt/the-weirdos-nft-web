af75cf8b70b7db5be57d750fb9067f09ad599016
windahidayatt <windahidayat27@gmail.com>
2022-04-18 10:23:58 +0700

    feat(home): Home section as initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "styled-components";
+import Footer from "./components/Footer";
+import Navigation from "./components/Navigation";
+import About from "./components/sections/About";
+import Faq from "./components/sections/Faq";
+import Home from "./components/sections/Home";
+import Roadmap from "./components/sections/Roadmap";
+import Showcase from "./components/sections/Showcase";
+import Team from "./components/sections/Team";
+import GlobalStyles from "./styles/GlobalStyles";
+import { light } from "./styles/Themes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <GlobalStyles/>
+    <ThemeProvider theme={light}>
+      <Navigation/>
+      <Home />
+      <About />
+      <Roadmap />
+      <Showcase />
+      <Team />
+      <Faq />
+      <Footer />
+    </ThemeProvider>
+    </>
   );
 }
 
